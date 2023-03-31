06392435320f427c255e9fcbd326510fcd67c314
Vitalii <witaliy_sapriga@ukr.net>
2023-03-31 23:54:07 +0300

    Working with Dynamic Inline Styling

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,29 +1,47 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+    const [inputText, setInputText] = useState("");
+    const [isInputValid, setIsInputValid] = useState(true);
 
-  const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
-  };
+    const taskInputChangeHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsInputValid(true);
+        }
+        setInputText(event.target.value);
+    };
 
-  const formSubmitHandler = (event) => {
-    event.preventDefault();
-    props.onAddTask(inputText);
-  };
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        if (inputText.trim().length === 0) {
+            setIsInputValid(false);
+            return
+        }
+        props.onAddTask(inputText);
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
-      </div>
-      <Button type="submit">Добавить Задачу</Button>
-    </form>
-  );
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label style={
+                    {color: !isInputValid ? 'red' : 'black'}
+                }>
+                    Задачи
+                </label>
+                <input style={{
+                    borderStyle: "solid",
+                    borderColor: !isInputValid ? 'red' : 'black',
+                    background: !isInputValid ? 'salmon' : 'transparent '
+                }}
+                       type="text"
+                       onChange={taskInputChangeHandler}/>
+            </div>
+            <Button type="submit">Добавить Задачу</Button>
+        </form>
+    );
 };
 
 export default TaskInput;
